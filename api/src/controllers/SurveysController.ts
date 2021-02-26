1d862ca4a654d22539f53582be83e7605201d4bd
ZumbiPy <zumbipy@gmail.com>
2021-02-26 13:42:31 -0300

    Craindo funçao show em SurveysControlle e uma get show em router

--- api/src/controllers/SurveysController.ts
+++ api/src/controllers/SurveysController.ts
@@ -14,11 +14,15 @@ class SurveysController {
         });
 
         await surveyRepository.save(survey);
-
         return response.status(201).json(survey)
-
-
     }
+
+    async show(request:Request, response:Response) {
+        const surveyRepository = getCustomRepository(SurveysRepository);
+        const all = await surveyRepository.find();
+
+        return response.json(all)
+        }
 }
 
 export { SurveysController }
