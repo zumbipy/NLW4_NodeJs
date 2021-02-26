11f19cb92c57606f424a8fc41474307de6b7729e
ZumbiPy <zumbipy@gmail.com>
2021-02-26 13:07:05 -0300

    Criando models Survey.ts

---
+++ api/src/models/Survey.ts
@@ -0,0 +1,29 @@
+import { Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+
+class Survey {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+    
+    @Column()
+    title: string;
+    
+    @Column()
+    description: string;
+    
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id){
+            this.id = uuid();
+        }
+    }
+
+}
+
+export { Survey }
