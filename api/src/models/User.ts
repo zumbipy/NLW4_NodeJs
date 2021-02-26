b6073e58c90f4efa8cc6dc080e917763b226c218
ZumbiPy <zumbipy@gmail.com>
2021-02-26 02:32:19 -0300

    Criando o Model User do projeto

---
+++ api/src/models/User.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+    @PrimaryColumn()
+    readonly id: string;  // faz com que nosso codigo crie essa string
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_id: Date
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+}
+
+export { User };
