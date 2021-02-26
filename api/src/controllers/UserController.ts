d1e37ea7969c3674a2b7c07fc2c05579c1522df8
ZumbiPy <zumbipy@gmail.com>
2021-02-26 02:50:28 -0300

    Criando regras de negocio para não te email repetido

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -4,11 +4,18 @@ import { User } from "../models/User";
 
 
 class UserController {
-    async create(request: Request, response: Response){
-        const {name, email} = request.body;
-        
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
         const usersRepository = getRepository(User);  // Responsável por manipular o bando de dados.
-        
+        const userAlreadyExists = await usersRepository.findOne({ email })  // Faça uma busca por email na tabela.
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                erro: "Usuario Já existe!!!"
+            })
+        }
+
         // Obrigadorio a criar para pode salva.
         const user = usersRepository.create({
             name,
