d4cea6b7e1fca41a921fac8b630bafe944526315
ZumbiPy <zumbipy@gmail.com>
2021-02-26 16:42:52 -0300

    Test User completo

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -20,7 +20,7 @@ describe("Users", () => {
 
       it("Should not be able to create a user with exist email", async() => {
           const response = await  request(app).post("/users").send({
-              email: "user@exemple.com",
+              email: "user@example.com",
               name: "User Example",
           });
           expect(response.status).toBe(400);
