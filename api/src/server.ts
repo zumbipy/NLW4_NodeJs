cf52aa80b87cf54aea0ac9078a85c728ff7a10d8
ZumbiPy <zumbipy@gmail.com>
2021-02-26 00:52:58 -0300

    Criando as routas post e get

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,4 +2,16 @@ import express from 'express';
 
 const app = express()
 
+app.get("/", (request, response) =>{
+return response.json({
+    messagem: "Hello World - NLW04"
+    })
+})
+
+app.post("/", (request, response) => {
+    return response.json({
+        messagem: "Os dados foram salvos com sucesso!"
+    })
+})
+
 app.listen(3333, () => console.log("Servidor Funcionando"))
