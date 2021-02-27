40d2fb57bed777f35b08381a2c8357c821ae0fab
ZumbiPy <zumbipy@gmail.com>
2021-02-27 11:49:56 -0300

    Refatoração do SendMailController

--- api/src/controllers/SendMailController.ts
+++ api/src/controllers/SendMailController.ts
@@ -5,6 +5,7 @@ import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
+import { SurveyUser } from '../models/SurveyUser';
 
 
 class SendMailController {
@@ -30,30 +31,36 @@ class SendMailController {
             })
         }
 
+
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs")
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: {user_id: user.id, value: null},
+            relations:["user", "survey"]
+        });
+
         const variables = {
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: "",
             link: process.env.URL_MAIL,
         };
-        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs")
-
-        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{user_id: user.id}, {value: null}],
-            relations:["user", "survey"]
-        });
 
         if(surveyUserAlreadyExists){
+            variables.id =surveyUserAlreadyExists.id;
             await SendMailService.execute(email, survey.title, variables, npsPath)
             return response.json(surveyUserAlreadyExists)
         }
+
+        
 
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
             survey_id
         });
         await surveysUsersRepository.save(surveyUser);
+        variables.id = surveyUser.id
 
         await SendMailService.execute(email, survey.title, variables, npsPath)
 
