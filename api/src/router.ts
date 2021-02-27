c73bcb8eb2fd6e1aa77af7ff38b232e98d15c515
ZumbiPy <zumbipy@gmail.com>
2021-02-27 12:25:45 -0300

    Criando Controller Aswers validados o Usuario e alteramos a nota

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -7,10 +8,11 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
-router.post("/send", sendMailController.execute)
-
+router.post("/send", sendMailController.execute);
+router.get("/answers/:value", answerController.execute);
 export { router };
