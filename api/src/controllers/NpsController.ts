47c0e2e47141e5daa90b9901e7f9872936a36c80
ZumbiPy <zumbipy@gmail.com>
2021-02-27 13:05:06 -0300

    Finalizado e rodado nps tudo ok

--- api/src/controllers/NpsController.ts
+++ api/src/controllers/NpsController.ts
@@ -1,10 +1,41 @@
-import { Request, Response } from "express";
+import { Request, Response, } from "express";
+import { getCustomRepository,  Not, IsNull } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 
 
 class NpsController {
     async execute(request: Request, response: Response) {
-        
+        const { survey_id } = request.params;
+        const surseysUserRepository = getCustomRepository(SurveysUsersRepository);
 
+        const surveysUsers = await surseysUserRepository.find({
+            survey_id,
+            value: Not(IsNull())
+        })
+
+        const detractor = surveysUsers.filter(survey =>
+            survey.value >= 0 && survey.value <= 6
+        ).length;
+
+        const promoters = surveysUsers.filter(survey =>
+            survey.value >= 9 && survey.value <= 10
+        ).length;
+
+        const passive = surveysUsers.filter(survey =>
+            survey.value >= 7 && survey.value <= 8
+        ).length;
+
+        const totalAnswers = surveysUsers.length;
+
+        const calculo =( (promoters - detractor) / totalAnswers ) * 100
+
+        return response.json({
+            detractor,
+            promoters,
+            passive,
+            totalAnswers,
+            nps: Number(calculo.toFixed(2)),
+        })
     }
 }
 
--- api/src/router.ts
+++ api/src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -9,10 +10,13 @@ const userController = new UserController();
 const surveyController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
+router.get("/nps/:survey_id", npsController.execute)
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 router.post("/send", sendMailController.execute);
 router.get("/answers/:value", answerController.execute);
+
 export { router };
