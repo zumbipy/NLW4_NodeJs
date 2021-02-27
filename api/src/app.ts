49996452809b0d976c4cc3f2b1a467851f7bd062
ZumbiPy <zumbipy@gmail.com>
2021-02-27 14:04:35 -0300

    Finalizando tudo

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 import createConnection from "./database"
 import { router } from './router';
 import { AppErros } from './errors/AppErros';
